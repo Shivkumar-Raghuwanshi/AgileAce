4111b5f7285128ba1d61f830c98a0a583639f5e2
Shivkumar-Raghuwanshi <shivkumarraghuwanshi56@gmail.com>
2024-01-28 08:26:29 +0530

    Update  9

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -18,9 +18,7 @@ const OrganizationIdPage = async () => {
       <Info isPro={isPro} />
       <Separator className="my-4" />
       <div className="px-2 md:px-4">
-        <Suspense fallback={<BoardList.Skeleton />}>
           <BoardList />
-        </Suspense>
       </div>
     </div>
   );
